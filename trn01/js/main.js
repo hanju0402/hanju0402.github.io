47f6dc71e1dd18ae58368ca2bddf53f8fb982eea
hanju0402 <104571007+hanju0402@users.noreply.github.com>
2022-05-04 16:02:23 +0900

    main.js 5/4

--- trn01/js/main.js
+++ trn01/js/main.js
@@ -2,12 +2,18 @@
 //    document.querySelector('.header').style.display ='none';
 // })
 
-$(function(){
-    $('.main_slider').slick({
-        arrows: false,
-        autoplay: true,
-        autoplaySpeed: 1000,
-        vertical: true,
-        dots: true,
-    });
-})
+$(function () {
+  $(".main_slider").slick({
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    vertical: true,
+    dots: true,
+  });
+
+  $(".mopen").on("click", function () {
+    $(".gnb").toggleClass("off");
+    $(this).toggleClass("off");
+    $(".header").toggleClass("off");
+  });
+});
