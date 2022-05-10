6657f4712a47d39f581c2d5e334fdb1ac919a908
hanju0402 <104571007+hanju0402@users.noreply.github.com>
2022-05-10 13:46:12 +0900

    Add files via upload

---
+++ trn02/js/main.js
@@ -0,0 +1,50 @@
+$(function(){
+    //////////////////////////////////////
+
+    $('.top_banner i').on('click', function(){
+        $('.top_banner').slideUp(1000);
+    });
+
+    $('.main_slider').on('init reInit afterChange', function(){
+        let current = $('.slick-current');
+        current.addClass('on').siblings().removeClass('on');
+    })
+
+    $('.main_slider').slick({
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+    });
+
+    $('.movie .dec i:nth-of-type(2)').on('click', function(){
+        $('.movie video').trigger('play')
+    });
+    $('.movie .dec i:nth-of-type(1)').on('click', function(){
+        $('.movie video').trigger('pause')
+    });
+
+    $('#myMovie').YTPlayer({
+              videoURL:'https://youtu.be/DWAJ_dm-A34',
+              containment:'.utube',
+              autoPlay:true, 
+              mute:true, 
+              startAt:0, 
+              opacity:1,
+              showControls: false,
+              playOnlyIfVisible: true,
+    });
+
+    $('.utube i:nth-of-type(2)').on('click', function(){
+        $('#myMovie').YTPPlay();
+    });
+
+    $('.utube i:nth-of-type(1)').on('click', function(){
+        $('#myMovie').YTPPause();
+    });
+
+
+
+    /////////////////////////////////////
+    });
